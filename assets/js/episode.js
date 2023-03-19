3529f2292ca6909dfe15b149ded89e7ddf179eaa
Killiane Letellier <killiane.letellier@mailo.com>
2023-03-19 10:05:25 +0100

    Links are now permalinks !

--- assets/js/episode.js
+++ assets/js/episode.js
@@ -10,13 +10,15 @@ const rawFeed = xmlHttp.responseText.split("\n").slice(1).join('\n');
 const jsonFeed = x2js.xml_str2json(rawFeed);
 const items = jsonFeed.rss.channel.item;
 
-const item = items[document.location.search.split("=")[1]];
-console.log(item)
+console.log(items)
+
+const item = items.filter(item => item.enclosure._url.includes(document.location.search.split("=")[1]))[0];
 
 window.onload = function() {
     document.title = `${item.title[1]} | Jennifer Letellier`;
     document.getElementById("audio").src = item.enclosure._url;
     document.getElementById("title").innerText = item.title[1];
     document.getElementById("description").innerHTML = item.description.split("------")[0].slice(0, -3);
+    document.getElementById("cover").src = (item.image) ? item.image._href : "https://media.redcircle.com/images/2022/4/3/16/62e79485-42d5-408e-9976-f482fee52c31_0d56e0b6-12c3-4d22-a71e-5f5a7f5a48bf_logo.jpg";
     document.getElementById("date").innerText = `${days[new Date(item.pubDate).getDay()]} ${new Date(item.pubDate).getDate()} ${months[new Date(item.pubDate).getMonth()]} ${new Date(item.pubDate).getFullYear()}`;
 }
--- assets/js/home.js
+++ assets/js/home.js
@@ -11,17 +11,17 @@ const jsonFeed = x2js.xml_str2json(rawFeed);
 const items = jsonFeed.rss.channel.item;
 
 window.onload = function () {
-    items.forEach((item, id) => {
+    items.forEach(item => {
         const newElement = document.createElement("div");
         newElement.className = "episodeItem";
         newElement.innerHTML = `
-            <img class="cover" src="https://media.redcircle.com/images/2022/4/3/16/62e79485-42d5-408e-9976-f482fee52c31_0d56e0b6-12c3-4d22-a71e-5f5a7f5a48bf_logo.jpg">
+            <img class="cover" src="${(item.image) ? item.image._href : "https://media.redcircle.com/images/2022/4/3/16/62e79485-42d5-408e-9976-f482fee52c31_0d56e0b6-12c3-4d22-a71e-5f5a7f5a48bf_logo.jpg"}">
             <div class="meta">
                 <h2>${item.title[1]}</h2>
                 <p>${days[new Date(item.pubDate).getDay()]} ${new Date(item.pubDate).getDate()} ${months[new Date(item.pubDate).getMonth()]} ${new Date(item.pubDate).getFullYear()}</p>
             </div>
         `;
-        newElement.onclick = function () { document.location.href = `/episode/?episode=${id}`; };
+        newElement.onclick = function () { document.location.href = `/episode/?episode=${item.enclosure._url.split("/")[4]}`; };
 
         document.getElementById("episodesList").appendChild(newElement);
     });
